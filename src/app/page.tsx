ffe1150873bf38b3aac576be9d84fc905d6c9a62
Byron Wang <byronwang123919@gmail.com>
2023-10-14 13:36:59 -0700

    initial work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { useAudioRecorder } from 'react-audio-voice-recorder';
-import styles from './page.module.css'
+import { useEffect, useState } from "react";
+import { useAudioRecorder } from "react-audio-voice-recorder";
+import styles from "./page.module.css";
 
 export default function Home() {
-
-  const {
-    startRecording,
-    stopRecording,
-    recordingBlob
-  } = useAudioRecorder();
+  const { startRecording, stopRecording, recordingBlob } = useAudioRecorder();
 
   // debug function
   const addAudioElement = (blob: any) => {
@@ -24,12 +19,25 @@ export default function Home() {
   async function handleTranscription(blob: any) {
     // audio file -> openAI whisper -> text
 
-    const response = await fetch(`https://api.openai.com/v1/audio/transcriptions`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_KEY}`,
+    const formData = new FormData();
+    const file = new File([blob], "audio.webm", { type: "webm/audio" });
+
+    formData.append("model", "whisper-1");
+    formData.append("file", file);
+
+    const response = await fetch(
+      `https://api.openai.com/v1/audio/transcriptions`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_KEY}`,
+        },
+        body: formData,
       }
-    });
+    );
+
+    const data = await response.json();
+    console.log(data, " is data");
   }
 
   const [recording, setRecording] = useState<boolean>(false);
@@ -39,32 +47,36 @@ export default function Home() {
       // currently speaking, stop and process
       stopRecording();
 
-      setRecording(false);  
+      setRecording(false);
     } else {
       // not recording, start speaking
       startRecording();
-      
-      setRecording(true)
+
+      setRecording(true);
     }
-  }
+  };
 
   useEffect(() => {
     if (!recordingBlob) return;
 
     // recordingBlob will be present at this point after 'stopRecording' has been called
-    console.log(recordingBlob)
+    console.log(recordingBlob);
     addAudioElement(recordingBlob);
-  }, [recordingBlob])
+
+    const testFunc = async () => {
+      await handleTranscription(recordingBlob);
+    };
+
+    testFunc();
+  }, [recordingBlob]);
 
   return (
     <div className={styles.container}>
-
-      
       <div className={styles.floatBottom}>
         <div className={styles.toggle} onClick={handleToggle}>
           {recording ? "Stop speaking" : "Start speaking"}
         </div>
       </div>
     </div>
-  )
+  );
 }
